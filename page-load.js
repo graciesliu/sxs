3cebe7745205f346d9d2da1be6817d6dd20addc7
Gracie <graceliu@wustl.edu>
2019-08-15 20:06:29 -0500

    carousels implemented

--- page-load.js
+++ page-load.js
@@ -11,19 +11,14 @@
          $('.about-invert').toggleClass('invertIcon');
          $('#fp-nav,.top-nav,#fullpage').fadeToggle(1000);
      });
-     
-//     
-//     $('.collapse').on('show.bs.collapse', function(e) {
-//  var $card = $(this).closest('.card');
-//  var $open = $($(this).data('parent')).find('.collapse.show');
-//  
-//  var additionalOffset = 0;
-//  if($card.prevAll().filter($open.closest('.card')).length !== 0)
-//  {
-//		additionalOffset =  $open.height();
-//  }
-//  $('html,body').animate({
-//    scrollTop: $card.offset().top - additionalOffset
-//  }, 500);
-//});
+
+//     $('.collapse').on('show.bs.collapse', function (e) {
+//         var o = $('.test').position();
+//        console.log("position: "+ o.top); 
+//         
+//         $('html,body').animate({
+//             scrollTop: 0
+//         }, 500);
+//         console.log("animated");
+//     });
  });
